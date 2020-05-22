9f86fa31895c53f6164188762b4550d3dbf0093a
selamsew amha <33477042+selamsewamha@users.noreply.github.com>
2020-05-22 21:45:58 +1000

    Update common.py

--- getmyip/common/common.py
+++ getmyip/common/common.py
@@ -4,14 +4,15 @@ import os
 import sys
 
 def setup_logger(level):
-  logging.basicConfig(level=level)
+    logging.basicConfig(level=level)
 
 def get_url():
-  built_path = os.path.join(sys.prefix, 'configurations', 'getmyip.yaml')
-  dev_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'configurations', 'getmyip.yaml'))
-  if os.path.exists(built_path):
-    path = built_path
-  else:
-    path = dev_path
-  with open(path, 'r') as f:
-    return yaml.load(f)['url']
+    built_path = os.path.join(sys.prefix, 'configurations', 'getmyip.yaml')
+    dev_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'configurations', 'getmyip.yaml'))
+    if os.path.exists(built_path):
+        path = built_path
+    else:
+        path = dev_path
+    with open(path, 'r') as f:
+        return yaml.load(f)['url']
+    
